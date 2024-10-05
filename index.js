f471813b2ca57ca64fe4c7a21da7f5724e4a4a9e
Sachin kumar jha <sachinkumarjha9080@gmail.com>
2024-10-06 02:21:00 +0530

    env updated

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
-if(process.env.NODE_ENV != "productrion"){
-    require('dotenv').config();
-}
 
+require('dotenv').config();
 const express=require("express");
 const app=express();
 const ejs=require('ejs');
