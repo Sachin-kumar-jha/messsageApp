ae54a7f81d134d201e1a39187d6a606c15620757
Sachin kumar jha <sachinkumarjha9080@gmail.com>
2024-10-06 15:42:21 +0530

    footer added and some changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,8 @@ const {ApiError}=require('../utils/error.js');
 
 const registerUser=async(req,res,next)=>{
    let {username,name,mobileNumber,email,password}=req.body;
+   const user=User.find({username:username,email:email,mobileNumber:mobileNumber});
+   if(user) return res.render("Error.ejs",{error:new ApiError(401,"User is already exists! please click the icon to register!")});
    const hash = bcrypt.hashSync(password,10);
  const newUser=new User({
    username,
@@ -13,10 +15,10 @@ const registerUser=async(req,res,next)=>{
    mobileNumber,
    password:hash,
  });
- try {
+ try{
     const savedUser= await newUser.save();
     //res.status(200).json(savedUser);
- } catch (error) {
+ }catch (error) {
     next(error);
  }
  next();
@@ -26,11 +28,11 @@ const registerUser=async(req,res,next)=>{
     let {email}=req.body;
     try {
         const user= await User.findOne({email});
-        if(!user) return next(new ApiError(404,"User not found"));
+        if(!user) return res.render("Error.ejs",{error:new ApiError(401,"User not found!")});
         let hash= user.password;
         let isPasswordCorrect=bcrypt.compareSync(req.body.password, hash);
         
-        if(!isPasswordCorrect) return next(new ApiError(404,"something went wrong"));
+        if(!isPasswordCorrect) return res.render("Error.ejs",{error:new ApiError(401,"Somethin went wrong!")});
 
 const token=jwt.sign({id:user._id,name:user.name},process.env.JWT);
         const {password,mobileNumber,...otherDetails}=user._doc;
